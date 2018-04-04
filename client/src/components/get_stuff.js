200b5defe53ae9330de4c68c177a3bc017ae5b4e
Duy Nguyen <duyng93@gmail.com>
2018-04-04 16:46:17 -0700

    Completed mern project demo

--- client/src/components/get_stuff.js
+++ client/src/components/get_stuff.js
@@ -4,8 +4,12 @@ import axios from 'axios';
 class GetStuff extends Component{
 
     componentDidMount(){
-        axios.get('http://localhost:9000/api/get-stuff').then(( resp ) =>{
+        axios.get('/api/get-stuff').then(( resp ) =>{
             console.log('response from server:', resp );
+        });
+
+        axios.post('/api/get-user').then((resp)=> {
+            console.log('response from get user:', resp);
         });
     }
 
--- index.js
+++ index.js
@@ -1,20 +1,61 @@
 const express = require('express');
 const cors = require('cors');
+const { resolve } = require('path');
 const PORT = process.env.PORT||9000;
 
 const app = express();
 
 app.use(cors());
 
+app.use(express.static(resolve(__dirname, 'client', 'dist')));
+
+//example of middleware
+// function log(req, res, next) {
+//     console.log('THIS IS A MIDDLEWARE');
+//     next();
+// }
+
+function log(msg) {
+    return(req, res, next)=>{
+        console.log(msg);
+        next();
+    }
+}
+
+function getUser() {
+    return(req, res, next) => {
+        req.user = {name: 'invisible', age: 27};
+        next();
+    }
+}
+
+//all routes
+//app.use(log);
+
+//log only get-stuff route
+   
 app.get('/api/get-stuff', (req, res) =>{
     res.send({
         success: true, message: "Here is the stuff from backend"
     });
+});
+
+app.get('*', (req, res) =>{
+    res.sendFile(resolve(__dirname, 'client', 'dist', 'index.html'));
 })
 
-app.get('/', (req, res) => {
-    res.send('<h1>This is something new....</h1>')
+app.post('/api/get-user', getUser(), (req, res) => {
+    // res.send({
+    //     success: true, user: {name: "invisible", age: 88}
+    // });
+
+    console.log('User:', req.user);
+    res.send({success: true, user: req.user});
 });
+
+// app.get('/', (req, res) => {
+//     res.send('<h1>This is something new....</h1>')
+// });
 
 app.listen(PORT, ()=>{
     console.log("Server running on port:" + PORT);
